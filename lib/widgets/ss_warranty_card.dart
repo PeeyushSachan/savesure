1e828e4285987ad03ebfe19bc8ead65e493d6fed
peeyushsachan <sachana766@gmail.com>
2025-03-29 14:38:48 +0530

    card design is done

--- lib/widgets/ss_warranty_card.dart
+++ lib/widgets/ss_warranty_card.dart
@@ -52,76 +52,204 @@ class _SsWarrantyCardState extends State<SsWarrantyCard> {
                 ),
               ),
 
-              // Content on top
-              Positioned.fill(
-                child: Column(
+              Positioned(
+                top: 12.5,
+                left: 12.5,
+                child: Row(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 1, right: 1),
-                          child: Stack(children: [
-                            /*  Container(
-                              height: 34,
-                              width: 117,
-                              decoration: BoxDecoration(color: Colors.blue), need ot be look on this
-                            ),*/
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(30),
-                                      topRight: Radius.circular(10))),
-                              height: 34,
-                              width: 117,
-                              child: Center(
-                                child: Text(
-                                  "Warrenty",
-                                  style: TextStyle(
-                                      color: Color(0xD9FF4703), fontSize: 16),
-                                ),
-                              ),
-                            ),
-                          ]),
-                        )
-                      ],
+                    Transform.scale(
+                      scale: 2.33, //42 pixels
+                      child: Checkbox(
+                          checkColor: Colors.white,
+                          value: isChecked,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(21)),
+                          fillColor: isChecked == true
+                              ? WidgetStateProperty.all(Color(0XFF4CAF50))
+                              : WidgetStateProperty.all(
+                                  Color.fromARGB(255, 255, 255, 255)),
+                          side: BorderSide(
+                              width: 1,
+                              color: const Color.fromARGB(255, 218, 87, 5)),
+                          splashRadius: 5,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              isChecked = value!;
+                            });
+                          }),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          width: 42,
-                          height: 42,
-                          child: Transform.scale(
-                            scale: 2.33,
-                            child: Checkbox(
-                                checkColor: Colors.white,
-                                value: isChecked,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(21)),
-                                fillColor: isChecked == true
-                                    ? WidgetStateProperty.all(Color(0XFF4CAF50))
-                                    : WidgetStateProperty.all(
-                                        Color.fromARGB(255, 255, 255, 255)),
-                                side: BorderSide(
-                                    width: 1,
-                                    color: const Color.fromARGB(
-                                        255, 172, 170, 170)),
-                                splashRadius: 5,
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    isChecked = value!;
-                                  });
-                                }),
-                          ),
-                        )
-                      ],
-                    ),
-                    Row(),
-                    Row(),
-                    Row(),
+                    SizedBox(width: 3),
+                    RichText(
+                        text: TextSpan(
+                            text: "23 ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                            children: [
+                          TextSpan(
+                              text: "Days Left \n",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.normal),
+                              children: [
+                                TextSpan(
+                                    text: "Expiry Date: 30 Jan 25",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.normal)),
+                              ])
+                        ]))
                   ],
                 ),
+              ),
+              // Content on top
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 1, right: 1),
+                        child: Stack(children: [
+                          /*  Container(
+                            height: 34,
+                            width: 117,
+                            decoration: BoxDecoration(color: Colors.blue), need ot be look on this
+                          ),*/
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(30),
+                                    topRight: Radius.circular(10))),
+                            height: 34,
+                            width: 117,
+                            child: Center(
+                              child: Text(
+                                "Warrenty",
+                                style: TextStyle(
+                                    color: Color(0xD9FF4703), fontSize: 16),
+                              ),
+                            ),
+                          ),
+                        ]),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 25, right: 12.5),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Laptop Buy Warrenty Card",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                "hardware issue, and report for an additional  2 years includes free services and 24/7 customer support",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          spacing: 10,
+                          children: [
+                            SizedBox(
+                              height: 30,
+                              width: 85,
+                              child: OutlinedButton(
+                                  onPressed: () {},
+                                  style: OutlinedButton.styleFrom(
+                                      elevation: 5,
+                                      side: BorderSide(color: Colors.white),
+                                      padding: EdgeInsets.all(0),
+                                      backgroundColor:
+                                          Color.fromARGB(75, 49, 40, 40)),
+                                  child: Text(
+                                    "View Details",
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: 32,
+                              width: 32,
+                              child: IconButton.outlined(
+                                style: OutlinedButton.styleFrom(
+                                    elevation: 5,
+                                    side: BorderSide(
+                                        color: Color.fromARGB(255, 218, 87, 5)),
+                                    padding: EdgeInsets.all(0),
+                                    backgroundColor: Colors.white),
+                                onPressed: () {},
+                                icon: Icon(Icons.download),
+                                iconSize: 17,
+                                color: Color.fromARGB(255, 218, 87, 5),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30,
+                              width: 85,
+                              child: OutlinedButton(
+                                  onPressed: () {},
+                                  style: OutlinedButton.styleFrom(
+                                      elevation: 5,
+                                      side: BorderSide(color: Colors.white),
+                                      padding: EdgeInsets.all(0),
+                                      backgroundColor:
+                                          Color.fromARGB(75, 49, 40, 40)),
+                                  child: Text(
+                                    "Edit",
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: 32,
+                              width: 32,
+                              child: IconButton.outlined(
+                                style: OutlinedButton.styleFrom(
+                                    elevation: 5,
+                                    side: BorderSide(
+                                        color: Color.fromARGB(255, 218, 87, 5)),
+                                    padding: EdgeInsets.all(0),
+                                    backgroundColor: Colors.white),
+                                onPressed: () {},
+                                icon: Icon(Icons.delete),
+                                iconSize: 17,
+                                color: Color.fromARGB(255, 218, 87, 5),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ],
           ),
