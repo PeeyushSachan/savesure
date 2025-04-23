feb2f9858fa4c490b75f2be4f8b4543117bbc0f0
peeyushsachan <sachana766@gmail.com>
2025-04-23 09:45:44 +0530

    LoginResponsive is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:savesure/view/auth/forgot_password_screen.dart';
-import 'package:savesure/view/auth/login_screen.dart';
-import 'package:savesure/view/auth/otp_verification_screen.dart';
-import 'package:savesure/view/auth/register_screen.dart';
-import 'package:savesure/view/auth/login_screen.dart';
-import 'package:savesure/view/auth/success_reset_screen.dart';
 
-import 'package:savesure/view/home/home_screen.dart';
+import 'package:savesure/view/auth/login_screen.dart';
+
+
 import 'package:savesure/view/notification/cards_notification.dart';
-import 'package:savesure/widgets/test.dart';
-import 'package:savesure/widgets/test2.dart'; // आपकी स्क्रीन
+// आपकी स्क्रीन
 
 // ✅ Main Function (entry point)
 Future<void> main() async {
   WidgetsFlutterBinding
-      .ensureInitialized(); // Initialization से पहले जरूरी होता है
+      .ensureInitialized(); 
   await initializeNotifications(); // Plugin init function call
   await requestNotificationPermissionIfNeeded(); // ⬅️ ASK FOR PERMISSION HERE
-  runApp(Main()); // App run करना
+  runApp(Main()); // App run 
 }
 
 // ✅ Main App Widget
@@ -28,7 +23,7 @@ class Main extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:LoginScreen(), // आपकी main screen
+      home:LoginScreen(), // main screen
     );
   }
 }
--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_media/responsive_media.dart';
 import 'package:savesure/core/theme/app_colors.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -24,199 +25,222 @@ class _LoginScreenState extends State<LoginScreen> {
 
     final rm = ResponsiveMedia.instance; // Use after init(context)
     return Scaffold(
-      appBar: AppBar(),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: RichText(
-                  text: TextSpan(
-                text: "Login Account \n",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: rm.h1 * 1.5,
-                    color: Colors.black),
-                children: [
-                  TextSpan(
-                    text: "Please login into your account",
-                    style: TextStyle(fontSize: rm.h2, color: Colors.grey),
-                  )
-                ],
-              )),
-              height: rm.shortestSide * 0.2,
-              width: rm.screenWidth,
-            ),
-            Container(
-              height: rm.shortestSide * 0.18,
-              child: TextField(
-                style: TextStyle(color: AppColors.primaryOrange),
-                decoration: InputDecoration(
-                  hintText: "Enter Your Email",
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color: AppColors.primaryOrange)),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: rm.shortestSide * 0.06),
-                  prefixIcon: Icon(Icons.email, color: AppColors.primaryOrange),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-            ),
-            rm.gapL(),
-            Container(
-              height: rm.shortestSide * 0.18,
-              child: TextField(
-                style: TextStyle(color: AppColors.primaryOrange),
-                obscuringCharacter: "*",
-                obscureText: passwordVisible,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide(color: AppColors.primaryOrange)),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: rm.shortestSide * 0.06),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  prefixIcon: Image.asset(
-                    "assets/Vector.png",
-                  ),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          passwordVisible = !passwordVisible;
-                        });
-                      },
-                      icon: Icon(passwordVisible
-                          ? Icons.visibility_off_outlined
-                          : Icons.visibility_outlined)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                      15,
-                    ),
-                  ),
-                ),
-                keyboardType: TextInputType.visiblePassword,
-                textInputAction: TextInputAction.done,
-              ),
-            ),
-            rm.gapL(),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                  height: rm.shortestSide * 0.05,
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                        color: Colors.deepOrange),
-                  )),
-            ),
-            rm.gapL(),
-            Container(
-              height: rm.shortestSide * 0.18,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Color(0xFFFA6C12), Color(0xFFC64AA1)]),
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.amber,
-              ),
-              child: Center(
-                child: Text(
-                  "Submit",
-                  style:
-                      TextStyle(color: Colors.white, fontSize: rm.button * 1.5),
-                ),
-              ),
-            ),
-            Container(
-              height: rm.shortestSide * 0.1,
-              child: Center(
-                child: Center(
-                    child: Text(
-                  "or login with",
-                  style: TextStyle(color: Colors.grey),
+        child: Padding(
+          padding: rm.horizontalPadding,
+          child: Column(
+            children: [
+              rm.gapXL(),
+              SizedBox(
+                width: rm.screenWidth,
+                child: RichText(
+                    text: TextSpan(
+                  text: "Login Account \n",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: rm.h1 * 1.5,
+                      color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: "Please login into your account",
+                      style: TextStyle(fontSize: rm.h2, color: Colors.grey),
+                    )
+                  ],
                 )),
               ),
-            ),
-            Container(
-              height: rm.shortestSide * 0.25,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: rm.shortestSide * 0.3,
-                    height: rm.shortestSide * 0.13,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
+              rm.gapL(),
+              SizedBox(
+                height: rm.shortestSide * 0.18,
+                child: TextField(
+                  style: TextStyle(color: AppColors.primaryOrange),
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    hintStyle: TextStyle(fontSize: rm.caption),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: AppColors.primaryOrange)),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: rm.shortestSide * 0.06),
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: AppColors.primaryOrange,
+                      size: rm.caption,
+                    ),
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
-                    child: Image.asset("assets/Group.png"),
                   ),
-                  Container(
-                    width: rm.shortestSide * 0.3,
-                    height: rm.shortestSide * 0.13,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Image.asset("assets/Facebook.png"),
-                  ),
-                  Container(
-                    width: rm.shortestSide * 0.3,
-                    height: rm.shortestSide * 0.13,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Image.asset("assets/Apple.png"),
-                  )
-                ],
-              ),
-            ),
-            Container(
-                child: Center(
-              child: RichText(
-                  text: TextSpan(
-                      style: TextStyle(color: Colors.grey, fontSize: rm.h3),
-                      children: <TextSpan>[
-                    TextSpan(text: 'By "Login Account", you agree to the'),
-                    TextSpan(
-                        text: ' Terms of Use',
-                        style: TextStyle(
-                            color: Color(0xFFC64AA1),
-                            fontWeight: FontWeight.bold)),
-                    TextSpan(text: " \n                    and"),
-                    TextSpan(
-                        text: "  Privacy Policy",
-                        style: TextStyle(
-                            color: Color(0xFFC64AA1),
-                            fontWeight: FontWeight.bold))
-                  ])),
-            )),
-            Container(
-              height: rm.shortestSide * 0.2,
-              child: Center(
-                child: RichText(
-                  text: TextSpan(
-                      style: TextStyle(color: Colors.grey, fontSize: rm.h2),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: "Don't have an account?",
-                            style: TextStyle(color: Colors.black)),
-                        TextSpan(
-                            text: " Create Account",
-                            style: TextStyle(
-                                color: Color(0xFFFA6C12),
-                                fontWeight: FontWeight.bold))
-                      ]),
                 ),
               ),
-            ),
-          ],
+              rm.gapM(),
+              SizedBox(
+                height: rm.shortestSide * 0.18,
+                child: TextField(
+                  style: TextStyle(color: AppColors.primaryOrange),
+                  obscuringCharacter: "*",
+                  obscureText: passwordVisible,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    hintStyle: TextStyle(fontSize: rm.caption),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: AppColors.primaryOrange)),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: rm.shortestSide * 0.06),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.lock,
+                      color: AppColors.primaryOrange,
+                      size: rm.caption,
+                    ),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            passwordVisible = !passwordVisible;
+                          });
+                        },
+                        icon: Icon(
+                            size: rm.caption,
+                            passwordVisible
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        15,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.done,
+                ),
+              ),
+              rm.gapS(),
+              Align(
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                    height: rm.shortestSide * 0.05,
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: rm.h4,
+                          color: Colors.deepOrange),
+                    )),
+              ),
+              rm.gapL(),
+              Container(
+                height: rm.shortestSide * 0.18,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Color(0xFFFA6C12), Color(0xFFC64AA1)]),
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.amber,
+                ),
+                child: Center(
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(
+                        color: Colors.white, fontSize: rm.buttonText * 1.5),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: rm.shortestSide * 0.1,
+                child: Center(
+                  child: Center(
+                      child: Text(
+                    "or login with",
+                    style: TextStyle(fontSize: rm.h3, color: Colors.grey),
+                  )),
+                ),
+              ),
+              SizedBox(
+                height: rm.shortestSide * 0.25,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        width: rm.shortestSide * 0.28,
+                        height: rm.shortestSide * 0.15,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Icon(
+                          FontAwesomeIcons.github,
+                          size: rm.shortestSide * 0.1,
+                          color: Colors.black,
+                        )),
+                    Container(
+                        width: rm.shortestSide * 0.28,
+                        height: rm.shortestSide * 0.15,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Icon(
+                          Icons.facebook,
+                          size: rm.shortestSide * 0.1,
+                          color: Colors.blue,
+                        )),
+                    Container(
+                        width: rm.shortestSide * 0.28,
+                        height: rm.shortestSide * 0.15,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Icon(
+                          Icons.apple,
+                          size: rm.shortestSide * 0.1,
+                        ))
+                  ],
+                ),
+              ),
+              Center(
+                child: RichText(
+                    text: TextSpan(
+                        style: TextStyle(color: Colors.grey, fontSize: rm.h3),
+                        children: <TextSpan>[
+                      TextSpan(text: 'By "Login Account", you agree to the'),
+                      TextSpan(
+                          text: '\nTerms of Use ',
+                          style: TextStyle(
+                              color: Color(0xFFC64AA1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: rm.h3)),
+                      TextSpan(text: " and "),
+                      TextSpan(
+                          text: "Privacy Policy",
+                          style: TextStyle(
+                              color: Color(0xFFC64AA1),
+                              fontWeight: FontWeight.bold))
+                    ])),
+              ),
+              SizedBox(
+                height: rm.shortestSide * 0.2,
+                child: Center(
+                  child: RichText(
+                    text: TextSpan(
+                        style: TextStyle(color: Colors.grey, fontSize: rm.h2),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: "Don't have an account?",
+                              style: TextStyle(
+                                  fontSize: rm.h2, color: Colors.black)),
+                          TextSpan(
+                              text: " Create Account",
+                              style: TextStyle(
+                                  color: Color(0xFFFA6C12),
+                                  fontWeight: FontWeight.bold))
+                        ]),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
