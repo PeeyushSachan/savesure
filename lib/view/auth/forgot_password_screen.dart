03e85975731ca674d3b0af44f783247cfe095691
peeyushsachan <sachana766@gmail.com>
2025-04-23 12:51:20 +0530

    forgot password is done

--- lib/view/auth/forgot_password_screen.dart
+++ lib/view/auth/forgot_password_screen.dart
@@ -1,159 +1,153 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:responsive_media/responsive_media.dart';
+import 'package:savesure/core/theme/app_colors.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
-  const ForgotPasswordScreen({super.key});
+  const ForgotPasswordScreen({Key? key}) : super(key: key);
 
   @override
-  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+  _ForgotPasswordScreenState createState() => _ForgotPasswordScreenState();
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+    ResponsiveMedia.init(context); // insitalization
 
+    final rm = ResponsiveMedia.instance; // Use after init(context)
     return Scaffold(
-      appBar: AppBar(),
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.05,
-          vertical: screenHeight * 0.08,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  child: Image.asset(
-                    "assets/image 5.png",
-                    width: screenWidth * 0.15, // Scales dynamically
-                  ),
+        child: Padding(
+          padding: rm.horizontalPadding,
+          child: Column(
+            children: [
+              rm.gapXL(),
+              rm.gapXL(),
+              Container(
+                child: Row(
+                  spacing: rm.spacingS,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: rm.shortestSide * 0.15,
+                      child: Image.asset(
+                        "assets/image 5.png",
+
+                        fit: BoxFit.cover,
+                        // Scales dynamically
+                      ),
+                    ),
+                    Container(
+                      child: RichText(
+                          text: TextSpan(
+                              text: "Forget Your Password? \n",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: rm.shortestSide *
+                                    0.06, // Adjusts dynamically
+                              ),
+                              children: [
+                            TextSpan(
+                              text: "Please Enter Your Email Address",
+                              style:
+                                  TextStyle(fontSize: rm.shortestSide * 0.04),
+                            )
+                          ])),
+                    )
+                  ],
                 ),
-                SizedBox(width: screenWidth * 0.03),
-                /*   Expanded(
-                  child: SizedBox(
-                  
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Forget Your Password?",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: screenWidth * 0.06, // Adjusts dynamically
-                          ),
-                        ),
-                        SizedBox(height: screenHeight * 0.005),
-                        Text(
-                          "Please Enter Your Email Address",
-                          style: TextStyle(fontSize: screenWidth * 0.04),
-                        ),
-                      ],
+              ),
+              rm.gapL(),
+              SizedBox(
+                height: rm.shortestSide * 0.18,
+                child: TextField(
+                  style: TextStyle(
+                      fontSize: rm.h1, color: AppColors.primaryOrange),
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    hintStyle: TextStyle(fontSize: rm.caption),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide(color: AppColors.primaryOrange)),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: rm.shortestSide * 0.06),
+                    prefixIcon: Padding(
+                      padding: rm.defaultPadding,
+                      child: Icon(
+                        Icons.email,
+                        color: AppColors.primaryOrange,
+                        size: rm.shortestSide * 0.08,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                ), */
-
-                Expanded(
-                  child:SizedBox(
-
-                    width: screenWidth*0.8,
-                    
-                    child: RichText(
-                        text: TextSpan(
-                            text: "Forget Your Password? \n",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: screenWidth * 0.06, // Adjusts dynamically
-                            ),
-                            children: [
-                          TextSpan(
-                            text: "Please Enter Your Email Address",
-                            style: TextStyle(fontSize: screenWidth * 0.04),
-                          )
-                        ])),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(height: screenHeight * 0.04),
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(screenWidth * 0.02),
-                  child: Image.asset(
-                    "assets/mail.png",
-                    width: screenWidth * 0.05,
+                ),
+              ),
+              Container(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Need Help?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: rm.shortestSide * 0.04,
+                      color: Colors.deepOrange,
+                    ),
                   ),
                 ),
-                border: OutlineInputBorder(
+              ),
+              rm.gapXL(),
+              Container(
+                width: double.infinity,
+                height: rm.shortestSide * 0.18,
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFFA6C12),
+                      Color(0xFFC64AA1)
+                    ], // Define gradient colors
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.02),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Need Help?",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: screenWidth * 0.04,
-                  color: Colors.deepOrange,
-                ),
-              ),
-            ),
-            SizedBox(height: screenHeight * 0.2),
-            Container(
-              width: double.infinity,
-              height: screenHeight * 0.06,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFFA6C12),
-                    Color(0xFFC64AA1)
-                  ], // Define gradient colors
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.transparent,
-                ),
-                onPressed: () {},
-                child: Text(
-                  "Continue",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: screenWidth * 0.045,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: rm.shortestSide * 0.045,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: screenHeight * 0.02),
-            SizedBox(
-              width: double.infinity,
-              height: screenHeight * 0.06,
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+              rm.gapL(),
+              SizedBox(
+                width: double.infinity,
+                height: rm.shortestSide * 0.18,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Back",
+                    style: TextStyle(fontSize: rm.shortestSide * 0.045),
                   ),
                 ),
-                onPressed: () {},
-                child: Text(
-                  "Back",
-                  style: TextStyle(fontSize: screenWidth * 0.045),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
