69e5d954b683b2df4aabc4cccae167dccdab0d3c
peeyushsachan <sachana766@gmail.com>
2025-05-16 04:32:59 +0000

    changes were maide

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,5 +4,14 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryOrange = Color(0Xffff660d);
-  static const Color borderOrange = Color(0XFF8E3CAB);
+  static const Color borderOrange = Color(0xFFFF8E3C);
+  static const Color backgroundOrange= Color(0x75FF8E3C);
+  static const Gradient buttonGradient=  LinearGradient(
+                      colors: [Color(0xFFFA6C12), Color(0xFFC64AA1)]);
+
+
+
+static const Color primaryGray= Color(0xFF524D4D);
+
+ 
 }
--- lib/test.dart
+++ lib/test.dart
@@ -20,66 +20,107 @@ class Test extends StatelessWidget {
           height: 560,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.all(Radius.circular(15)),
-              border: Border.all(color: AppColors.primaryOrange)),
+              border: Border.all(color: AppColors.borderOrange)),
           child: Column(
             children: [
                 
-              Container(
-                    child: rm.gapM(),
-                   ),
-             Padding(
-              padding: rm.paddingHorizontal(3),
-                child: Row(
+            
+             Container(
+          padding:EdgeInsets.symmetric( vertical: 15),
+               color: const Color.fromARGB(117, 246, 215, 194),
+               child: Padding(
+                padding: rm.paddingHorizontal(3),
+                  child: Row(
+                    
+                    children: [
                   
-                  children: [
-                
-                    SizedBox(
-
-                      width: 40,
-                      height: 40,
-                      child: ElevatedButton(
-                                      
-                                      
-                                      
-                        
-                        style: ElevatedButton.styleFrom(
-                              
-                              padding: EdgeInsets.zero,
-                          shape: RoundedRectangleBorder(
-                                      
-                                 
-                            borderRadius: BorderRadius.circular(8)
-                          )
-                        ),
-                        onPressed: (){}, child:Icon(FontAwesomeIcons.lessThan, size: 15,) ,),
-                    ),
-                
-                   
-                    Expanded(
-                
-                     
-                      child: Center(
-                        child: Row(
+                      SizedBox(
+                  
+                        width: 40,
+                        height: 40,
+                        child: ElevatedButton(
+                                        
+                                        
+                                        
                           
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              FontAwesomeIcons.bell,
-                              color: AppColors.primaryOrange,
-                              size: 20,
-                            ),
-                          rm.gapXS(isHorizontal: true),
-                            Text(
-                              "Notification",
-                              style: TextStyle(fontSize: rm.h4),
+                          style: ElevatedButton.styleFrom(
+                                
+                                padding: EdgeInsets.zero,
+                            shape: RoundedRectangleBorder(
+                                        
+                                   
+                              borderRadius: BorderRadius.circular(8)
                             )
-                          ],
-                        ),
+                          ),
+                          onPressed: (){}, child:Icon(FontAwesomeIcons.lessThan, size: 15,) ,),
                       ),
-                    )
-                  ],
+                  
+                     
+                      Expanded(
+                  
+                       
+                        child: Center(
+                          child: Row(
+                            
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                FontAwesomeIcons.bell,
+                                color: AppColors.primaryOrange,
+                                size: 20,
+                              ),
+                            rm.gapXS(isHorizontal: true),
+                              Text(
+                                "Notification",
+                                style: TextStyle(fontSize: rm.h4, fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              )
+             ),
+
+
+             Container(
+
+              height: 36,
+          color:     AppColors.backgroundOrange,
+          child: Center(
+
+            child: Text("14 New Notifications", style: TextStyle(
+
+              color: AppColors.primaryOrange
+            ),),
+          ),
+             ),
+             
+             Expanded(
+               child: ListView.builder
+               (itemCount: 5,
+               
+               itemBuilder: (index , context)
+
+               
+               {
+                
+                
+               
+               return ListTile(
+               
+                 title: Text("Upcoming Event: Tech Expo 2025!"),
+               
+                 subtitle: Text("You just made a transfer of 45.00 to Trevor Philips on November 18, 2024 at 09:31 AM. If you did not do this, please call 11223344 immediately."),
+              
+              trailing: Icon(FontAwesomeIcons.greaterThan , size: 12, color: Colors.black,),
+              
+               );
+               
+               
+               }),
+             )
             ],
           ),
         ),
