2f661f9ad386b502254c64a1625ba5d59051056e
peeyushsachan <sachana766@gmail.com>
2025-06-13 05:27:23 +0000

    chages are made but edit pages has some issue

--- lib/view/home/tab/action/add_new_card.dart
+++ lib/view/home/tab/action/add_new_card.dart
@@ -25,56 +25,255 @@ class AddNewCard extends StatelessWidget {
                 ),
               ],
             ),
+            rm.gapL(),
 
-  rm.gapL(),
-Row(
 
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-  children: [
-     Text('Type of Card: ' ,style: TextStyle(
-        fontWeight: FontWeight.w700,
+            rm.gapM(),
 
-      fontSize: rm.h3
-     ),),
+                  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Title',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Enter The Product Name",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            rm.gapM(),
+rm.gapM(),
 
-   
-  rm.gapS( isHorizontal: true),
-            Expanded(
-              child: TextField(
-  //    controller: Controller,
-      style: TextStyle(fontSize: rm.h3, color: AppColors.primaryOrange),
-      decoration: InputDecoration(
-        hintText: "Choose card type",
-        hintStyle: TextStyle(fontSize: rm.h3,
-      
-        
-    ),
-        focusedBorder: OutlineInputBorder(
-      //      borderRadius: BorderRadius.circular(15),
-            borderSide: BorderSide(color: AppColors.primaryOrange)),
-        contentPadding: 
-            EdgeInsets.symmetric(vertical: rm.shortestSide * 0.03, horizontal: rm.shortestSide*0.02),
-        suffixIcon: Padding(
-          padding: rm.paddingHorizontal(3),
-          child: Icon(
-           Icons.arrow_drop_down,
-            color: AppColors.primaryOrange,
-            size: rm.shortestSide * 0.08,
-          ),
-        ),
-        border: OutlineInputBorder(
+                  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Description',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      maxLines: 4,
+                      keyboardType: TextInputType.multiline,
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Enter The Product Description",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            rm.gapM(),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Type of Card: ',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Choose card type",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        suffixIcon: Padding(
+                          padding: rm.paddingHorizontal(3),
+                          child: Icon(
+                            Icons.arrow_drop_down,
+                            color: AppColors.primaryOrange,
+                            size: rm.shortestSide * 0.08,
+                          ),
+                        ),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            rm.gapM(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Purchase Data: ',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      keyboardType: TextInputType.datetime,
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Enter The Date OF Purchase",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            rm.gapM(),
+
+
+                        Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Expriy Data: ',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      keyboardType: TextInputType.datetime,
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Enter The Date Of Expiry",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            rm.gapM(),
+
+
+                  Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    'Expriy Data: ',
+                    style:
+                        TextStyle(fontWeight: FontWeight.w700, fontSize: rm.h2),
+                  ),
+                ),
+                rm.gapS(isHorizontal: true),
+                Expanded(
+                    flex: 5,
+                    child: TextField(
+                      keyboardType: TextInputType.datetime,
+                      //    controller: Controller,
+                      style: TextStyle(
+                          fontSize: rm.h3, color: AppColors.primaryOrange),
+                      decoration: InputDecoration(
+                        hintText: "Enter The Date Of Expiry",
+                        hintStyle: TextStyle(
+                          fontSize: rm.h3,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            //      borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(color: AppColors.primaryOrange)),
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: rm.shortestSide * 0.03,
+                            horizontal: rm.shortestSide * 0.02),
+                        border: OutlineInputBorder(),
+                      ),
+                    ))
+              ],
+            ),
+            
+
+
+
+
+
          
-        ),
-      ),
-    )
-            )
-  ],
-),
-
-
-           
-           
           ],
         ),
       ),
