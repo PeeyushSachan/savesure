b43f6f25431d065ce3374f9d1842463ca9be02a5
peeyushsachan <sachana766@gmail.com>
2025-06-19 05:28:32 +0000

    Making full view for warr

--- lib/view/home/tab/cardAction/full_card_view.dart
+++ lib/view/home/tab/cardAction/full_card_view.dart
@@ -11,33 +11,93 @@ const FullCardView({ Key? key }) : super(key: key);
     final rm = ResponsiveMedia.instance;
     return Scaffold(
 
-      body: Column(
+      body: Container(
 
-        children: [
+        color: const Color.fromARGB(255, 241, 127, 61),
+        child: Padding(
+          padding: rm.paddingAll(5),
+          child: Column(
+          
+            children: [
+          
+          
+          
+          
+          
+          Container(
+          
+            
+          
+          width: rm.screenWidth,
+          height: rm.shortestSide*0.6,
+            child: Stack(
+              children:[
+                
+                
+             
+                 Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                        rm.gapL(),
+                        
+                        
+              SingleChildScrollView(
+                child: Image.network("https://m.media-amazon.com/images/I/61UMJm9fxOL._SY879_.jpg",
+                
+                         // height: 400,
+                width: rm.screenWidth*0.9,
+                 fit: BoxFit.cover,),
+              )
+                ],
+                        
+                        
+                        
+              ),
+
+
+                 Positioned(
+                  top:  10,
+                  right: 10,
+                   child: Container(
+                     
+           height: rm.shortestSide * 0.08,
+                   width: rm.shortestSide*0.20  ,
+                    decoration: BoxDecoration(
+                     color: AppColors.primaryOrange,
+                   borderRadius: BorderRadius.circular(5),
+
+                    ),
+                    child: Center(
+                      child: Text(
+                        "30 Days Left",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: rm.buttonText ),
+                      ),
+                    ),
+                                   ),
+                 ),
 
 
 
-Container(
 
-  color: AppColors.backgroundOrange,
-
-width: rm.screenWidth,
-height: 800,
-  child: Stack(
-
-    children: [
-
-
-  Image.network("https://m.media-amazon.com/images/I/61UMJm9fxOL._SY879_.jpg", fit: BoxFit.fill,)
-    ],
-
-
-
-  ),
-
-
-)
-        ],
+                    Positioned(
+                  bottom:  5,
+                  right: 10,
+                   child: Container(
+                     
+           
+                    
+                    child: IconButton(onPressed: (){}, icon: Icon(Icons.fullscreen , size: 25, color: AppColors.primaryOrange,)),
+                                   ),
+                 ),
+              ],
+            ),
+          
+          
+          )
+            ],
+          ),
+        ),
       ),
 
 
